f57b4ad00c744ec0d697d664da2481657590b14b
Student <example@example.com>
2020-01-15 11:50:29 -0800

    add form and define variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
+$("#appointmentForm").submit(function(){
+    var firstName = $("input#firstName").val();
+    var lastName = $("input#lastName").val();
+    var description = $("textarea#description").val();
+    var date = $("input#date").val();
+    var hourSelection = $("select#hourSelection").val();
+    var minSelection = $("select#minSelection").val();
+    var AMPMSelection = $("select#AMPMSelection").val();
 
+    $(".firstName").text(firstName);
+    $(".lastName").text(lastName);
+    // $(".description").text(description);
+    $(".date").text(date);
+    $(".hourSelection").text(hourSelection);
+    $(".minSelection").text(minSelection);
+    $(".2").text(AMPMSelection);
+
+
+
+    $(".confirmation").show();
+
+
+    console.log(firstName,lastName,description, date,hourSelection, minSelection, AMPMSelection)
+    event.preventDefault();
+})
 });
